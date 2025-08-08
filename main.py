fa95cf4943ecc7a961b257bf20cd6e20be4472ca
woutheijne <w.h.m.heijne@student.tue.nl>
2025-08-08 10:52:12 +0200

    Cleaned up main program

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from arp import *
-from dns import *
-from sslstrip import *
 from GUI import *
-import time
 
-TARGET_IP = "10.0.2.5"
-SPOOFED_IP = "10.0.2.6"
-IFACE = "enp0s8"
-
-ssl = sslstrip()
-ssl.start(TARGET_IP, IFACE)
-
-#GUI()
+GUI()
