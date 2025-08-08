0e4e2ae445592d1e9db128e7410cdeb475f3d713
wheijne <w.h.m.heijne@student.tue.nl>
2025-08-08 11:19:55 +0200

    cleanup

--- page.py
+++ page.py
@@ -2,8 +2,20 @@ import Tkinter as tk
 import threading
 
 class page(tk.Frame):
+    """
+    Represents a page on which an attack can be started.
+    Need to override:
+    - add_info_labels for the explaination of the attack.
+    - add_entries for any extra inputs.
+    - get_and_validate_inputs to retrieve the inputs and make sure they are correct. This function must return the arguments for the attacks start function.
+    - start_text which gets displayed when the attack is started and takes the same order of arguments of the attacks start function.
+    """
     
     def __init__(self, parent, title, attack_module):
+        """
+        Create an instance of the page.
+        attack module is an instance of the attack class, must contain a start and stop function.
+        """
         tk.Frame.__init__(self, parent)
         
         self.parent = parent
@@ -26,14 +38,23 @@ class page(tk.Frame):
         self.info_label = label(self, "")
     
     def add_info_labels(self):
+        """
+        Can be overriden to add info labels beneath the title.
+        """
         pass
     
     def add_entries(self):
+        """
+        Can be overriden to add more inputs, other than interface and target IP.
+        """
         self.interface = text_and_input(self, "Interface:")
         self.target_ip = text_and_input(self, "Target IP:")
         self.target_ip.config(validate="key", validatecommand=self.vcmd_ip)
     
     def handle_start(self):
+        """
+        Ran when the start button is clicked.
+        """
         args = self.get_and_validate_inputs()
         if not args:
             self.info_label.config(text="Please check you inputs")
@@ -45,9 +66,16 @@ class page(tk.Frame):
         self.stop_button.config(state="normal")
         
     def get_and_validate_inputs(self):
+        """
+        Must be overriden to get and validate inputs.
+        Must return the arguments of the start function.
+        """
         pass
         
     def handle_stop(self):
+        """
+        Stops the attack.
+        """
         self.stop_button.config(state="disabled")
         self.attack_module.stop()
         self.thread.join()
@@ -56,11 +84,17 @@ class page(tk.Frame):
         
 
 def label(parent, text):
+    """
+    Add a label.
+    """
     lbl = tk.Label(parent, text=text)
     lbl.pack(fill="x")    
     return lbl
     
 def text_and_input(parent, text):
+    """
+    Add an input with an info label.
+    """
     container = tk.Frame(parent)
     tk.Label(container, text=text, width=20).pack(side="left")
     entry = tk.Entry(container, width=20)
@@ -69,11 +103,17 @@ def text_and_input(parent, text):
     return entry
 
 def button(parent, text, handler):
+    """
+    Add a button.
+    """
     btn = tk.Button(parent, text=text, command=handler)
     btn.pack()
     return btn
     
 def validate_number(text):
+    """
+    Check whether the text is a positive number or an empty string
+    """
     if text == "":
         return True
     decimal_count = text.count(".")
@@ -84,6 +124,9 @@ def validate_number(text):
     return False
     
 def validate_ip(text):
+    """
+    Check whether the text is (the start of) a valid IP address.
+    """
     if text == "":
         return True
     splitted = text.split(".")
@@ -95,6 +138,6 @@ def validate_ip(text):
         if not t.isdigit():
             return False
         n = int(t)
-        if n > 255 or n < 0:
+        if n > 255 or n <= 0:
             return False
     return True
